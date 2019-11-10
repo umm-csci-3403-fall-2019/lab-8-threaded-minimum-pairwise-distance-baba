0a8ed749722db2a4b4fa2c3f566653903e255377
monjo003 <monjo003@morris.umn.edu>
2019-11-10 12:22:55 -0600

    Create and start the 4 threads and join the threads.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,42 @@
 package mpd;
 
-public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
 
+public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+    int globalResult = Integer.MAX_VALUE;
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+      //  throw new UnsupportedOperationException();
+         this.values = values;
+
+        Thread threadLowL = new Thread(new Lower_left());
+        threadLowL.start();
+        Thread threadTopR = new Thread(new Top_right());
+        threadTopR.start();
+        Thread threadBotR = new Thread(new Bottom_right());
+        threadBotR.start();
+        Thread threadC = new Thread(new center());
+        threadC.start();
+
+        try {
+            // joining the threads
+            threadLowL.join();
+            threadTopR.join();
+            threadBotR.join();
+            threadC.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return globalResult;
     }
 
-}
+    public synchronized int changeGlobalResult(int result){
+        if(globalResult > result){
+            globalResult = result;
+        }
+        return globalResult;
+    }
+
+    }
+
